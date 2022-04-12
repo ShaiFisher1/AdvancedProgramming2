7b5179a35a2426211fc9f2db84ba0e914fe25826
ShaiFisher1 <74471077+ShaiFisher1@users.noreply.github.com>
2022-04-12 22:39:12 +0300

    fixed import in App.js, added Back to Login button in RegisterationItem.js, added verifyPasswordRegisteration.js file (script for now)

--- App.js
+++ App.js
@@ -2,15 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Form from 'react-bootstrap/Form'
-import RegisterItem from './RegisterItem/RegisterItem';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginItem from './LoginItem/LoginItem';
+import RegisterationItem from './RegisterationItem/RegisterationItem.js';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="React FromApp">
+    <div class="opacity-75" className="App-decor">Regular shadow</div>
       <header className="App-header">
-        <div class="opacity-75" className="App-decor">Regular shadow</div>
-        <RegisterItem />
+      <Router>
+        <Routes>
+            <Route path='/RegisterationItem' element={<RegisterationItem/>}></Route>
+            <Route path='/' element={<LoginItem/>}></Route>
+        </Routes>
+      </Router>
       </header>
     </div>
   );
